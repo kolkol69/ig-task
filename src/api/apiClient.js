0cd6140a77a11f4cf24cced2bd7f33717c0d4290
Maks Kolodiy <kolodiy1103@gmail.com>
2023-05-28 17:55:05 +0200

    add proper base url instead of reading it from env

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,14 +2,9 @@ import axios from 'axios'
 
 import { setupInterceptorsTo } from './interceptors'
 
-export const baseUrl =
-  process.env.REACT_APP_BASE_URL === undefined
-    ? 'http://localhost:4000'
-    : process.env.REACT_APP_BASE_URL
-
 export const authApi = setupInterceptorsTo(
   axios.create({
-    baseURL: baseUrl,
+    baseURL: 'https://recruitmentdb-508d.restdb.io/rest',
     withCredentials: false,
   }),
 )
